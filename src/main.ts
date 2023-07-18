9d3cd565621aa868c1be34b494e8f0759f032b99
Fabian Hobi <fabian.hobi@bluewin.ch>
2023-07-18 22:01:23 +0200

    added kontra.js

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,32 @@
 // imports
 import './style.css'
+import { init, Sprite, GameLoop } from "kontra";
 
 console.log('Hello world!');
+
+// initialize
+let { canvas, context } = init();
+
+let sprite: Sprite = Sprite({
+    x: 100,
+    y: 80,
+    color: 'red',
+    width: 20,
+    height: 40,
+    dx: 2
+});
+
+let loop: GameLoop = GameLoop({
+    update: function(): void {
+        sprite.update();
+
+        if (sprite.x > canvas.width) {
+            sprite.x = -sprite.width;
+        }
+    },
+    render: function(): void {
+        sprite.render();
+    }
+});
+
+loop.start();
